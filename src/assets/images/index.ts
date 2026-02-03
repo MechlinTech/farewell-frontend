1745d4a7b831f35cafc8c288d23a3792bbd32910
mahak190525 <m.chhabra@mechlintech.com>
2026-02-03 21:31:34 +0530

    - Update imports and styles across multiple files - change image path in Babel and TypeScript config, adjust padding in HeadingGroupComponent, add SelectUserRoleScreen to navigation, and modify WelcomeScreen content.
    # Conflicts:
    #	src/assets/images/index.ts
    #	src/screens/InnerApp.tsx
    #	src/screens/WelcomeScreen/WelcomeScreen.tsx

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,6 @@
 const images = {
     package: require('./package.svg'),
     bike: require('./bike.svg'),
-
-    frame: require("./Frame.svg"),
-    farewell: require('./farewell.svg'),
 };
 
 export default images;
--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,26 +1,16 @@
 import * as React from 'react';
 import { StyleSheet, Text } from 'react-native';
-import { scale, verticalScale } from '@scale';
+import { scale } from '@scale';
 
 import BaseWrapper from 'components/Base';
-import color from '@color';
-import images from '@images';
-import ImageComponent from 'components/ImageComponent';
 
 const WelcomeScreen = () => {
-
-  
   return (
     <BaseWrapper
       container_style={styles.container}
       fullScreenMode={true}
     >
-
-      <ImageComponent
-        source={images.farewell}
-        style={styles.image}
-      />
-
+      <Text>233</Text>
     </BaseWrapper>
   );
 };
@@ -31,12 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: scale(50),
-    backgroundColor: color.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  image: {
-    width: scale(322),
-    height: verticalScale(142),
-  }
 });
