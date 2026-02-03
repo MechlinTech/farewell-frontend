8ba40d47195e026759a63ef7a44dedfb834f4812
MadhavBallabhKakkar <madhavkakkar75@gmail.com>
2026-02-04 02:30:39 +0530

    comitted by madhav

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,21 +1,26 @@
 import * as React from 'react';
-import { ImageComponent, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { scale, verticalScale } from '@scale';
 
 import BaseWrapper from 'components/Base';
 import color from '@color';
 import images from '@images';
+import ImageComponent from 'components/ImageComponent';
 
 const WelcomeScreen = () => {
+
+  
   return (
     <BaseWrapper
       container_style={styles.container}
       fullScreenMode={true}
     >
-      {/* <ImageComponent
+
+      <ImageComponent
         source={images.farewell}
         style={styles.image}
-      /> */}
+      />
+
     </BaseWrapper>
   );
 };
@@ -26,7 +31,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: scale(50),
-    backgroundColor: color.primary
+    backgroundColor: color.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
     width: scale(322),
