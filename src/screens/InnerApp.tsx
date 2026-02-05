ebb85bfc883a2b1cc845b90fd37537c9a71f2ada
mahak190525 <m.chhabra@mechlintech.com>
2026-02-06 02:10:30 +0530

    Refactor CustomButton and OTPVerificationScreen styles to use constants for font sizes and families; minor cleanup in HeadingGroupComponent and InnerApp error handling.
    # Conflicts:
    #	src/components/CustomButton.tsx
    #	src/screens/login/OTPVerificationScreen/OTPVerificationScreen.tsx

--- src/screens/InnerApp.tsx
+++ src/screens/InnerApp.tsx
@@ -320,7 +320,7 @@ export const checkApplicationPermission = async () => {
           }
         }
       }
-    } catch (error) {}
+    } catch (error) { }
   } else {
     try {
       const authStatus = await messaging().requestPermission();
